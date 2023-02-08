f68b1818abf4b52dd0b70667264ef9122f6af5c7
Sanjeev Pillai <sanjeev.pillai@accedo.tv>
2023-02-08 15:21:20 +0000

    create generic request function

---
+++ src/utils/makeRequest.ts
@@ -0,0 +1,18 @@
+export const makeRequest = (url: string, method = 'GET') => {
+	return new Promise<any>((resolve, reject) => {
+		const xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+
+		xhr.onload = () => {
+			if(xhr.status === 200) {
+				resolve(xhr)
+			} else {
+				const error = `Request failed. Returned status of ${xhr.status}`
+				reject({
+					status: xhr.status,
+					statusText: error
+				})
+			}
+		}
+	});
+};
